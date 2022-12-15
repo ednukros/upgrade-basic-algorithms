11eac7400bcb425fe0dd99a4c0fc5c518fceb6d6
ednukros <isanmenarguez@gmail.com>
2022-12-15 08:31:10 +0100

    Iteration#3

--- upgrade-basic-algorithms.js
+++ upgrade-basic-algorithms.js
@@ -54,4 +54,32 @@ console.log(toy1.price+toy2.price);
  car1.finalPrice = globalBasePrice + car1.basePrice;
  car2.finalPrice = globalBasePrice + car2.basePrice;
 
- console.log(car1.finalPrice,car2.finalPrice);
+//  1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+console.log(10*5);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+console.log(10/2);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+console.log(15%9);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+let a = 10;
+let b = 5;
+
+let c= a + b;
+console.log(c);
+
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+a = 10;
+b = 5;
+
+c = a*b;
+
+console.log(c);
