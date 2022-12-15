8b46586004e793d9a2ac233d61f7945effc788e7
ednukros <isanmenarguez@gmail.com>
2022-12-15 13:38:33 +0100

    iteration#4

--- upgrade-basic-algorithms.js
+++ upgrade-basic-algorithms.js
@@ -41,28 +41,37 @@ const toy2 = {
     price: 29
 };
 
-console.log(toy1.price+toy2.price);
+console.log(toy1.price + toy2.price);
 
 // 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 // y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
 // basePrice más el valor de la variable globalBasePrice.
- let globalBasePrice = 10000;
- const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
- const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+let globalBasePrice = 10000;
+const car1 = {
+    name: 'BMW m&m',
+    basePrice: 50000,
+    finalPrice: 60000
+};
+const car2 = {
+    name: 'Chevrolet Corbina',
+    basePrice: 70000,
+    finalPrice: 80000
+};
 
- globalBasePrice = 25000;
- car1.finalPrice = globalBasePrice + car1.basePrice;
- car2.finalPrice = globalBasePrice + car2.basePrice;
+globalBasePrice = 25000;
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
 
+//Iteración #3
 //  1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
 
-console.log(10*5);
+console.log(10 * 5);
 
 // 1.2 Divide 10 por 2 y muestra el resultado en un alert.
-console.log(10/2);
+console.log(10 / 2);
 
 // 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
-console.log(15%9);
+console.log(15 % 9);
 
 // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 // teniendo dos variables y = 10 y z = 5.
@@ -70,7 +79,7 @@ console.log(15%9);
 let a = 10;
 let b = 5;
 
-let c= a + b;
+let c = a + b;
 console.log(c);
 
 
@@ -80,6 +89,51 @@ console.log(c);
 a = 10;
 b = 5;
 
-c = a*b;
+c = a * b;
 
 console.log(c);
+
+
+//Iteración #4
+
+// 1.1 Consigue el valor "HULK" del array de cars y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers[0]);
+
+
+
+// 1.2 Cambia el primer elemento de avengers a "IRONMAN"
+//const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+avengers[0]="IRONMAN";
+console.log(avengers);
+
+// 1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+//const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+console.log(avengers.length);
+
+// 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+// Muestra en consola el último personaje del array
+
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length -1]);
+
+
+// 1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2[0]);
+//ECMAScript 2022 agrega at()
+console.log(rickAndMortyCharacters2.at(-1));
+
+
+// 1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+rickAndMortyCharacters3.splice(1,1);
+ console.log(rickAndMortyCharacters3);
+
