a27dcfb7581ca808a03307b291f92887e1c6aef5
ednukros <isanmenarguez@gmail.com>
2022-12-15 07:50:01 +0100

    Iteration#1

---
+++ upgrade-basic-algorithms.js
@@ -0,0 +1,8 @@
+1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+let myFavoriteHero = "Hulk";
+
+
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
